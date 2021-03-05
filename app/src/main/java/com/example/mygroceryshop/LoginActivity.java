149c6cd50725529c232cee24ac17c2262cea02b9
Isfak-Shakil <isfakshakil@gmail.com>
2021-03-05 12:16:24 +0600

    Login,RegisterSeller,rUser,ForgetPassword xml Activity

--- app/src/main/java/com/example/mygroceryshop/LoginActivity.java
+++ app/src/main/java/com/example/mygroceryshop/LoginActivity.java
@@ -2,13 +2,47 @@ package com.example.mygroceryshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText emailEt,passwordEt;
+    private TextView forgotTextView;
+    private Button loginbtn,signUpLogBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailEt=findViewById(R.id.email_log_Id);
+        passwordEt=findViewById(R.id.password_log_Id);
+        forgotTextView=findViewById(R.id.fogotPassword_Log_Id);
+        loginbtn=findViewById(R.id.login_btn_Id);
+        signUpLogBtn=findViewById(R.id.sign_up_Log_btn_Id);
+
+        signUpLogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterUserActivity.class));
+            }
+        });
+
+        forgotTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
